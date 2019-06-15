9ac42e89064584a3743577d96ce952e5d8082a03
AshwinRam2000 <ramashwin36@gmail.com>
2019-06-15 08:44:46 +0530

    flutter

---
+++ flask/demo/build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: D:\Flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
---
+++ flask/demo/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <permission/PermissionPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PermissionPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionPlugin"]];
+}
+
+@end
